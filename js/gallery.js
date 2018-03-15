bad2601873a2970a84f48013dbfbe1a5ab337666
aravindrk7 <33639370+aravindrk7@users.noreply.github.com>
2018-03-15 23:40:21 +0530

    deleted some files

--- js/gallery.js
+++ js/gallery.js
@@ -15,10 +15,10 @@ function run(){
 			console.log(youtube+response.data.items[i].id.videoId);
 			var videos = document.getElementById('videos');
 			var result = `<div class = "vid-holder" onclick="openPopup()">
-						   	 <a href = "${youtube+response.data.items[i].id.videoId+'?autoplay=1'}" target = "playhere">
+						   	 <a href = "${youtube+response.data.items[i].id.videoId+'?autoplay=1&color=white'}" target = "playhere">
 						    	<img src = "${response.data.items[i].snippet.thumbnails.medium.url}">
 						   	 	<div class = "content">
-						   	 		<h3>${response.data.items[i].snippet.channelTitle}</h3>
+						   	 		<h3>${response.data.items[i].snippet.title}</h3>
 						   	 		<p>${response.data.items[i].snippet.description}</p>
 						   	 	</div>
 						   	 </a>	
